913d573f80b73bef8ae90244456930bf1c0eef0f
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-12-01 11:46:50 +0200

    done_final

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,35 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const BASE_URL = `https://pixabay.com/api/`;
 const KEY = `40926027-5cb2084dfcf445810afb57cb9`;
 const curentIMG = 40;
 const photoCard = document.querySelector('.photo-card');
-const load = document.querySelector('.load-more');
 const search = document.querySelector('.search-form');
-const searchBtn = document.getElementById('searchButton');
 let currentPage = 1;
 let resultSearch;
 
-load.addEventListener('click', onLoud);
+let simpleGallery = new SimpleLightbox('.large-img', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+const target = document.querySelector('.js-guard');
+let options = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 1.0,
+};
 
 search.addEventListener('submit', onSearch);
 
 async function onSearch(event) {
   event.preventDefault();
-  currentPage=1
-  load.hidden = true
+  currentPage = 1;
+  observer.unobserve(target);
   const { searchQuery } = event.currentTarget.elements;
   resultSearch = searchQuery.value.trim();
   if (resultSearch.length === 0) {
@@ -27,17 +39,18 @@ async function onSearch(event) {
   try {
     const gallery = await getGallery(resultSearch, currentPage);
     if (curentIMG < gallery.totalHits) {
-      load.hidden = false;
+      observer.observe(target);
     }
-    if (gallery.totalHits === 0) { 
+    if (gallery.totalHits === 0) {
       return Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
+        '❌Sorry, there are no images matching your search query. Please try again.'
       );
     }
     photoCard.innerHTML = creatMarkup(gallery.hits);
     Notiflix.Notify.success(
-      'Hooray! We found ' + `${ gallery.totalHits }` + ' images.'
+      '✅ Hooray! We found ' + `${gallery.totalHits}` + ' images.'
     );
+    simpleGallery.refresh();
   } catch (err) {
     console.log(err);
   } finally {
@@ -45,23 +58,31 @@ async function onSearch(event) {
   }
 }
 
-async function onLoud() {
-  currentPage += 1;
-  try {
-    
-    const gallery = await getGallery(resultSearch, currentPage);
-    photoCard.insertAdjacentHTML('beforeend', creatMarkup(gallery.hits));
-    const totalPage = Math.ceil(gallery.totalHits / curentIMG);
-    console.log(`currentPage:`,currentPage);
-    if (currentPage === totalPage) {
-      load.hidden = true;
-      Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
+let observer = new IntersectionObserver(onLouder, options);
 
+function onLouder(entries, observer) {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+
+      try {
+        const gallery = await getGallery(resultSearch, currentPage);
+        photoCard.insertAdjacentHTML('beforeend', creatMarkup(gallery.hits));
+
+        simpleGallery.refresh();
+        const totalPage = Math.ceil(gallery.totalHits / curentIMG);
+        if (currentPage === totalPage) {
+          observer.unobserve(target);
+          Notiflix.Notify.warning(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+}
 async function getGallery(name, currentPage) {
   const params = new URLSearchParams({
     key: KEY,
@@ -87,7 +108,7 @@ function creatMarkup(arr) {
         views,
         comments,
         downloads,
-      }) => `<li class=info-item> <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      }) => `<li class=info-item>  <a class='large-img' href=${largeImageURL}>  <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
     <div class="info">
       <p class="info-item">
         <b>Likes</b>${likes}
